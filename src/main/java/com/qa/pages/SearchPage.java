b07893c77dfaa52fba3e9c0c328bee43f24c4fc8
mahesh <maheshkmr543@gmail.com>
2019-06-21 12:10:05 +0530

    added searcg page feature code

---
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -0,0 +1,11 @@
+package com.qa.pages;
+
+public class SearchPage {
+
+	public void searchPage() {
+		System.out.println("search page");
+	}
+	public void loginSet() {
+		System.out.println("login set");
+	}
+}
